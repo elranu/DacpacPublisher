ca4fde384eab2dcb3cbc2e6b58a2437cd9cf7460
Mariano Julio Vicario <elranu@gmail.com>
2019-05-30 18:42:48 -0300

    solution :rocket:

---
+++ SqlDacUIPublisher/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.ConnectionUI;
+using Microsoft.SqlServer.Dac;
+
+namespace SqlDacUIPublisher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_ConnectionString(object sender, RoutedEventArgs e)
+        {
+            // Display the connection dialog
+            var dlg = new DataConnectionDialog();
+            dlg.DataSources.Add(DataSource.SqlDataSource);
+            dlg.DataSources.Add(DataSource.SqlFileDataSource);
+
+            if (System.Windows.Forms.DialogResult.OK == DataConnectionDialog.Show(dlg))
+            {
+                ConnectionStringTextBox.Text = dlg.ConnectionString;
+            }
+        }
+
+        private void DacpacButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    DacpacTextBox.Text = file;
+                    DacpacTextBox.ToolTip = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    break;
+            }
+        }
+
+        private async void PublishButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DacpacTextBox.IsEnabled = false;
+                ConnectionStringTextBox.IsEnabled = false;
+                PublishButton.IsEnabled = false;
+                PublishButton.Content = "Publishing...";
+                ResultLabel.Content = "Result: ";
+                ResultTextBox.Text = string.Empty;
+
+                var dac = DacPackage.Load(DacpacTextBox.Text);
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(ConnectionStringTextBox.Text);
+                var dacService = new DacServices(ConnectionStringTextBox.Text);
+
+                var result = await Task.Run<PublishResult>(() => dacService.Publish(dac, builder.InitialCatalog, new PublishOptions()));
+                ResultTextBox.Text = result.DatabaseScript;
+            }
+            catch(Exception ex)
+            {
+                ResultTextBox.Text = ex.Message;
+                ResultLabel.Content = "Error: ";
+            }   
+            finally
+            {
+                DacpacTextBox.IsEnabled = true;
+                ConnectionStringTextBox.IsEnabled = true;
+                PublishButton.IsEnabled = true;
+                PublishButton.Content = "Publish";
+            }
+            
+        }
+    }
+}
